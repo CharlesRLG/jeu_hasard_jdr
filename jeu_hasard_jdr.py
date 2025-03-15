990bf71250c3348d5450ef004c5a38510ac57705
CharlesRLG <charlesrabeclegloahec@gmail.com>
2025-03-15 14:29:46 +0100

    mise en place du jeu à une pièce d'or

--- jeu_hasard_jdr.py
+++ jeu_hasard_jdr.py
@@ -1,10 +1,16 @@
 import random
 
-lootMiseA = [100,20,5,4,3,2,1,0,0,0]
+lootMiseA = [100,20,50,-4,3,0,-1,0,0,-5]
 lootMiseB = []
 lootMiseC = []
 choixRamdom = random.randint(0,9)
+gains = []
 
+def sommeGain (listeGain):
+    _sommeGain = 0
+    for i in listeGain:
+        _sommeGain = _sommeGain + i
+    return _sommeGain
 
 choix_de_la_mise = int(input("Combien voulez vous miser ? (1 PO, 10 PO, 100 PO) "))
 
@@ -16,12 +22,22 @@ if choix_de_la_mise == 1:
     if choixRamdom != 9:
         while choixRamdom != lootMiseA[7] or choixRamdom != lootMiseA[8]:
             gain = lootMiseA[choixRamdom]
-            print(gain)
             choixRamdom = random.randint(0,8)
-
+            gains.append(gain)
+            
     else: 
         print("Perdu ! veux tu rejouer ?")
+    #print(len(gains))
+    #print (sommeGain(gains))
+    sommeTotale = sommeGain(gains)
+    pieceOr = (sommeTotale / 20)
+    pieceArgent = sommeTotale % 20
 
+    print("Vous avez gagnez:")
+    print("- {0:6.0f}".format(pieceOr) + " Pièce d'Or")
+    print("- {0:6.0f}".format(pieceArgent) + " Pièce d'Argent")
+    print("")
+    print("Voulez-vous rejouer ?")
 
 elif choix_de_la_mise == 10:
     print ("Vous avez misé 10")
